cf7dbf66744e70d4f2ead2c04cda3f5d1a431eac
huang <167190968+justttthuang@users.noreply.github.com>
2025-01-11 01:17:18 +0800

    test

---
+++ src/main/java/org/ms/medicalsystem/MedicalsystemApplication.java
@@ -0,0 +1,13 @@
+package org.ms.medicalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ms/medicalsystem/MedicalsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.medicalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
